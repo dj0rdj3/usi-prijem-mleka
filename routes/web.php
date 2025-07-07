750adf736e1cabf2382b7d8e6192c844a15bebb9
DJ0RDJ3 <51521841+dj0rdj3@users.noreply.github.com>
2025-07-07 12:24:27 +0200

    uklonjen welcome i stavljen middleware na sve rute

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,26 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    if (auth()) {
+        return redirect('dashboard');
+    } else {
+        return redirect('login');
+    }
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+    Route::resource('users', App\Http\Controllers\UserController::class)->except('create', 'store', 'edit');
 
+    Route::resource('domacinstvo', App\Http\Controllers\DomacinstvoController::class)->except('index', 'edit');
 
-Route::resource('users', App\Http\Controllers\UserController::class)->except('create', 'store', 'edit');
+    Route::resource('radni-nalog', App\Http\Controllers\RadniNalogController::class)->except('edit');
 
-Route::resource('domacinstvo', App\Http\Controllers\DomacinstvoController::class)->except('index', 'edit');
+    Route::resource('preuzeto-mleko', App\Http\Controllers\PreuzetoMlekoController::class)->only('index', 'show');
+});
 
-Route::resource('radni-nalog', App\Http\Controllers\RadniNalogController::class)->except('edit');
-
-Route::resource('preuzeto-mleko', App\Http\Controllers\PreuzetoMlekoController::class)->only('index', 'show');
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
