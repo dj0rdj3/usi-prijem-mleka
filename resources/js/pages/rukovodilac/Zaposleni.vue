48300d2557bde83bfafa40a84e2f01df5294eff5
DJ0RDJ3 <51521841+dj0rdj3@users.noreply.github.com>
2025-07-09 10:53:18 +0200

    prikaz ukupno zaposlenih u tabeli zaposlenih

--- resources/js/pages/rukovodilac/Zaposleni.vue
+++ resources/js/pages/rukovodilac/Zaposleni.vue
@@ -17,7 +17,7 @@ const breadcrumbs = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col rounded-xl p-4 overflow-x-auto">
             <Heading title="Zaposleni" />
-            <table class="table text-left">
+            <table class="table text-left max-w-5xl">
                 <thead>
                     <tr class="bg-neutral-800">
                         <th class="rounded-tl-xl pl-2 leading-8">Ime i prezime</th>
@@ -30,7 +30,15 @@ const breadcrumbs = [
                     <tr v-for="z in zaposleni" class="even:bg-neutral-900/90 odd:bg-neutral-900/50 border-t-1">
                         <Zaposleni :zaposleni="z" />
                     </tr>
-                    <tr class="bg-neutral-800/80"><td colspan="4" class="rounded-b-xl">&nbsp;</td></tr>
+                    <tr class="bg-neutral-800/80">
+                        <td colspan="4" class="text-sm rounded-b-xl py-1.5 px-2">
+                            <div class="flex justify-around">
+                                <span>Ukupno zaposlenih: {{ zaposleni.length }}</span>
+                                <span>Ukupno vozača: {{ ukupnoVozaca }}</span>
+                                <span>Ukupno tehnologa: {{ ukupnoTehnologa }}</span>
+                            </div>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -43,6 +51,12 @@ import Zaposleni from '@/components/Zaposleni.vue'
 export default {
     props: {
         zaposleni: Object,
+    },
+    data() {
+        return {
+            ukupnoVozaca: this.zaposleni?.filter((z) => z?.tip === 'vozac').length,
+            ukupnoTehnologa: this.zaposleni?.filter((z) => z?.tip === 'tehnolog').length,
+        };
     },
 };
 </script>
